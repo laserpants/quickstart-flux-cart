a5a2527a549c471f9f4641fae31626f3a8060cfe
johanneshilden <hildenjohannes@gmail.com>
2017-09-26 01:32:19 +0300

    render Main component

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Main from './Main.jsx';
 
 ReactDOM.render(
-  <div>
-    Hello
-  </div>,
+  <Main />,
   document.getElementById('app')
 );
