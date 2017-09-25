3b6f4ca65de6c77119ba3c36faede4a1247eeeb0
johanneshilden <hildenjohannes@gmail.com>
2017-09-26 00:47:26 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/app.js'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'app.bundle.js'
+  },
+  devServer: {
+    inline: true,
+    contentBase: path.resolve(__dirname, './dist'),
+    port: 3000
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+};
