00f1800b75d80e0fef5ce903e7b3d76c0a06c17e
johanneshilden <hildenjohannes@gmail.com>
2017-09-27 19:08:07 +0300

    Implement cart actions

--- src/cart/Actions.js
+++ src/cart/Actions.js
@@ -3,15 +3,36 @@ import Dispatcher from './Dispatcher';
 import Articles from './stores/Articles';
 
 const Actions = {
-  addItem(key) {
+  addItem(key, quantity, article) {
+    const collection = Articles.getState();
+    const found = collection.get(key);
+    if (!article && found) {
+      article = found;
+    } else if (article && !found) {
+      Dispatcher.dispatch({
+        type: ActionTypes.INSERT_ARTICLE,
+        key,
+        article
+      });
+    }
     Dispatcher.dispatch({
       type: ActionTypes.ADD_ITEM,
+      key,
+      quantity,
+      article
+    });
+  },
+  removeItem(key) {
+    Dispatcher.dispatch({
+      type: ActionTypes.REMOVE_ITEM,
       key
     });
   },
-  removeItem() {
-  },
-  updateItem() {
+  updateItem(key) {
+    Dispatcher.dispatch({
+      type: ActionTypes.UPDATE_ITEM,
+      key
+    });
   },
   reset() {
     Dispatcher.dispatch({
